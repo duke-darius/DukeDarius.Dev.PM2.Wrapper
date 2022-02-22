455bd13e7287045a3abd88c0122e83b3d34a3039
Aaron <aaronjgriffin96@gmail.com>
2022-02-22 19:40:40 +0000

    added some basic tests :)

--- DukeDarius.Dev.PM2.Wrapper.Test/PM2.cs
+++ DukeDarius.Dev.PM2.Wrapper.Test/PM2.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
-
+using System;
+using System.Linq;
 
 namespace DukeDarius.Dev.PM2.Wrapper.Test
 {
@@ -11,10 +12,44 @@ namespace DukeDarius.Dev.PM2.Wrapper.Test
 
         }
 
+        
+
         [Test]
-        public void Test1()
+        public async void CanListProcesses()
         {
-            Assert.IsTrue(true);
+            var processes = await PM2.GetMonitoredProcesses();
+            Assert.IsNotNull(processes);
+        }
+
+        [Test]
+        public async void CanAddProcess()
+        {
+            var newProcess = await PM2.StartProcess(new Models.ProcessStartArgs(AppContext.BaseDirectory, "app1.js"));
+            Assert.IsNotNull(newProcess);
+        }
+
+        [Test]
+        public async void CanStopProcess()
+        {
+            var all = await PM2.GetMonitoredProcesses();
+            Assert.IsNotNull(all);
+
+            var first = all.FirstOrDefault();
+            Assert.IsNotNull(first);
+
+            await first.Stop();
+        }
+
+        [Test]
+        public async void CanStartProcess()
+        {
+            var all = await PM2.GetMonitoredProcesses();
+            Assert.IsNotNull(all);
+
+            var first = all.FirstOrDefault();
+            Assert.IsNotNull(first);
+
+            await first.Start();
         }
     }
 }
